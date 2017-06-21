7442a6c7644ca5486ea7c5f99f8429771f20f173
Emilien Chapon <emilien.chapon@cern.ch>
2017-06-21 23:26:10 +0200

    Working version

--- ttbar_pA8TeV/significance/testMyPFLHC.C
+++ ttbar_pA8TeV/significance/testMyPFLHC.C
@@ -11,6 +11,9 @@
 using namespace RooFit;
 using namespace RooStats;
 
+// GLOBAL SYST ON xsec
+double gGlobalSyst = 0.25;
+
 HypoTestResult* testMyPFLHC(const char *filename="finalfitworkskace_v2.root") {
    TFile *f = TFile::Open(filename);
    RooWorkspace *w = (RooWorkspace*) f->Get("w");
@@ -27,21 +30,61 @@ HypoTestResult* testMyPFLHC(const char *filename="finalfitworkskace_v2.root") {
    RooDataSet *data = (RooDataSet*) w->data("data");
    RooRealVar *thePoi = (RooRealVar*) w->var("xsec");
 
-   model->fitTo(*data,NumCPU(2),Minos(RooArgSet(*thePoi)),Extended(kTRUE),
-         ExternalConstraints(RooArgSet(ebconstraint)),GlobalObservables(RooArgSet(eb_nom))); // add minos
-   myPFLHC pl(*data,*model,RooArgSet(*thePoi));
+   // // without systematic uncertainty
+
+   // model->fitTo(*data,NumCPU(2),Minos(RooArgSet(*thePoi)),Extended(kTRUE),
+   //       ExternalConstraints(RooArgSet(ebconstraint)),GlobalObservables(RooArgSet(eb_nom))); // add minos
+
+   // myPFLHC pl(*data,*model,RooArgSet(*thePoi));
+   // // pl.SetGlobalObservables(RooArgSet(eb_nom));
+   // pl.SetNuisanceParameters(RooArgSet(*(w->var("eb"))));
+   // pl.SetMinos(true);
+   // pl.SetExtended(true);
+   // pl.SetNCPU(4);
+   // // pl.SetConstraints(RooArgSet(ebconstraint));
+   // pl.SetConstraints(new RooArgSet(*(w->function("ebconstraint"))));
+   // RooArgSet *nullParams = new RooArgSet(*thePoi);
+   // nullParams->setRealValue("xsec",0);
+   // pl.SetNullParameters(*nullParams);
+   // // pl.GetInterval();
+   // HypoTestResult *r = pl.GetHypoTest();
+   // r->Print();
+
+   // including systs with a global factor
+
+   RooRealVar syst_kappa("syst_kappa","syst_kappa",1+gGlobalSyst);
+   syst_kappa.setConstant(kTRUE);
+   w->import(syst_kappa);
+   w->factory( "expr::alpha_syst('pow(syst_kappa,beta_syst)',syst_kappa,beta_syst[0,-5,5])" );
+   w->factory( "Gaussian::constr_syst_pdf(beta_syst,glob_syst[0,-5,5],1)" );
+   w->factory( "expr::constr_syst('0.5*pow((@0-@1)/1,2)',beta_syst,glob_syst)" );
+   w->var("glob_syst")->setConstant(kTRUE);
+   w->var("glob_syst")->setVal(0);
+   w->factory("EDIT::model_combined_mjj_syst(model_combined_mjj,xsec=expr('xsec*alpha_syst',xsec,alpha_syst))");
+   w->Print();
+
+   RooArgSet *constraints_pdf = new RooArgSet(ebconstraint,*(w->pdf("constr_syst_pdf")));
+
+   RooAbsPdf *model_syst = w->pdf("model_combined_mjj_syst");
+   model_syst->fitTo(*data,NumCPU(2),Minos(RooArgSet(*thePoi)),Extended(kTRUE),
+         ExternalConstraints(*constraints_pdf)); // add minos
+
+   myPFLHC pl2(*data,*model_syst,RooArgSet(*thePoi));
    // pl.SetGlobalObservables(RooArgSet(eb_nom));
-   pl.SetNuisanceParameters(RooArgSet(*(w->var("eb"))));
-   pl.SetMinos(true);
-   pl.SetExtended(true);
-   pl.SetNCPU(4);
+   pl2.SetNuisanceParameters(RooArgSet(*(w->var("eb"))));
+   pl2.SetMinos(true);
+   pl2.SetExtended(true);
+   pl2.SetNCPU(4);
    // pl.SetConstraints(RooArgSet(ebconstraint));
-   pl.SetConstraints(new RooArgSet(*(w->function("ebconstraint"))));
+   RooArgSet *constraints = new RooArgSet(*(w->function("ebconstraint")),*(w->function("constr_syst")));
+   pl2.SetConstraints(constraints);
    RooArgSet *nullParams = new RooArgSet(*thePoi);
    nullParams->setRealValue("xsec",0);
-   pl.SetNullParameters(*nullParams);
+   pl2.SetNullParameters(*nullParams);
    // pl.GetInterval();
-   HypoTestResult *r = pl.GetHypoTest();
+   HypoTestResult *r = pl2.GetHypoTest();
    r->Print();
+
+
    return r;
 }
