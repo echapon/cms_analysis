867e4c526a848fc12678dbd646d60854bb8e8427
Emilien Chapon <emilien.chapon@cern.ch>
2017-06-29 13:29:26 +0200

    Add script for toys on batch

---
+++ ttbar_pA8TeV/significance/batch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+basedir=/afs/cern.ch/user/e/echapon/workspace/private/cms_analysis/ttbar_pA8TeV/significance
+source /afs/cern.ch/sw/lcg/external/gcc/4.9/x86_64-slc6-gcc49-opt/setup.sh; source /afs/cern.ch/sw/lcg/app/releases/ROOT/6.04.02/x86_64-slc6-gcc49-opt/root/bin/thisroot.sh
+
+workdir=/tmp/echapon/tmp_$SEED
+mkdir -p $workdir
+cd $workdir
+
+cp $basedir/*C $basedir/*root .
+
+root -l -b -q computeSignif.C'("finalfitworkspace_uncorrwjets_constantjsf_0.root",'$SEED')' 2>&1 | tail -n100 2>&1
+cp Freq* $basedir
